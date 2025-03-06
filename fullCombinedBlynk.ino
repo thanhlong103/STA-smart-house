c23c75451726ce2761556f92e9d963ba2078dcc4
crazyblueer <140491984+crazyblueer@users.noreply.github.com>
2025-03-06 19:54:31 +0700

    Update fullCombinedBlynk.ino

--- fullCombinedBlynk.ino
+++ fullCombinedBlynk.ino
@@ -340,20 +340,3 @@ void turnOffNeonLights() {
 }
 
 
-
-// #define LDR_PIN 25  // Connect LDR to GPIO34 (Any ADC pin)
-
-// void setup() {
-//     Serial.begin(115200);
-//     Serial.println("Light Sensor Initialized...");
-//     pinMode(LDR_PIN, INPUT);
-// }
-
-// void loop() {
-//     int lightValue = analogRead(LDR_PIN);  // Read analog value from LDR
-//     Serial.print("Light Intensity: ");
-//     Serial.println(lightValue);  // Print the value - 4000 is dark, 100 is light
-
-//     delay(1000);  // Wait 1 second before next reading
-// }
-
